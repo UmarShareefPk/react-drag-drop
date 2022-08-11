7fa68e0f1c56955949be79d8f96441c7f05e6a2e
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-08-12 02:38:13 +0500

    Update DragDrop.js

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -5,7 +5,7 @@ import "../App.css";
 
 const PictureList = [
     {
-      id: 1,
+      id: 1, 
       url:
         "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
     },
